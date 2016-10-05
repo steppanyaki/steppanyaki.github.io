63dcec112aaecee1bd7c2e39d3c6659d38567be0
steppanyaki <a0930794927@gmail.com>
2016-10-05 19:29:39 -0400

    test jquery event

---
+++ script.js
@@ -0,0 +1,11 @@
+/*
+window.onload = function () {
+    alert("welcome");
+};
+*/
+
+$(document).ready(function() {
+    $('div').mouseenter(function() {
+        $(this).fadeTo('fast', 1)
+    });
+});
